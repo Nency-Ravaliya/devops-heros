79d578844c15f5333b73575bdecc00560ce5664d
Khushboo Choudhary <khushboo@Khushboos-MacBook-Air.local>
2026-08-18 11:58:56 +0530

    Shell Scripting

---
+++ ShellProgramming/first.sh
@@ -0,0 +1,27 @@
+# mkdir hello
+# cd hello
+# touch app.log
+# echo "Hello, World!" > app.log
+# cat app.log
+
+# mkdir hello
+# cd hello
+# echo "line1" > app.log
+# cat app.log
+# echo "line2" > app.log
+# cat app.log
+
+# #!/bin/bash
+# variable1="Hello Dear"
+# variable2=142
+# variable3="Great class"
+# ls="my file!!"
+# echo "my dear , $variable1"
+# echo "my roll no $variable2"
+# echo "this class  is $variable3"
+# echo $ls
+
+# # !bin/bash
+# read -p "Enter your name: " name
+# read -p "Enter your age: " age
+# echo "Hello, $name! You are $age years old."
