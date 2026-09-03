f41f1a3003a1920669316616b684a1191e5b66ad
shubh <shubhshukla1006@gmail.com>
2026-09-03 23:16:28 +0530

    Complete Docker Fundamentals assignment with 6 Hello World apps

--- session6-7-docker/python-app/app.py
+++ session6-7-docker/python-app/app.py
@@ -1,1 +1,17 @@
-print("Hello World from Docker!")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello World from Python Application!</h1>")
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 5000)
+    httpd = server_class(server_address, handler_class)
+    print("Python HTTP Server running on port 5000...")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
