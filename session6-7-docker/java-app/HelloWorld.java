66b85521b2836132ed8a7c1c5ec3929e0040fb17
anshalkumar <anshalrai@yahoo.com>
2026-09-03 23:12:35 +0530

    Session 6-7: Docker hello-world apps (node/python/java/apache/react/nginx), multi-stage build verification, deployment docs

---
+++ session6-7-docker/java-app/HelloWorld.java
@@ -0,0 +1,30 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        int port = 8000;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new HelloHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server running on port " + port);
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "<h1>Hello World from Docker (Java)!</h1>";
+            exchange.getResponseHeaders().set("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
