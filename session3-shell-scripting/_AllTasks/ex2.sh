9e607f085a22daec7312619503e701c1d40d3430
Yadavdhruv1 <dhruvyadav132006@gmail.com>
2026-09-20 23:30:14 +0530

    Update ex2.sh

--- session3-shell-scripting/_AllTasks/ex2.sh
+++ session3-shell-scripting/_AllTasks/ex2.sh
@@ -1,6 +1,6 @@
 # Q : Make three variables, name, roll, comment then print them 
 
-name="Tirth "
-roll="420"
+name="Dhruv"
+roll="204"
 comm="devops"
 echo "Maru naam $name che, Maro roll no $roll che, hu $comm bhanu chu"
